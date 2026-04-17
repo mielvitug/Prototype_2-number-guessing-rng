5ee28807a0ff3391605e470bc7009536a3791b69
mielvitug <atvitug1@gmail.com>
2026-04-17 18:55:47 +0800

    Update

--- main.py
+++ main.py
@@ -12,16 +12,25 @@ secret_number = random.randint(1,100)
 
 # function show_guess
 def show_guess():
-    guess = guess_entry.get()
-    result_label.config(text=f"You typed: {guess}")
+    try: # attempts this code
+        guess = int(guess_entry.get())
+    except ValueError: # gets the specific error and returns with the statement
+        result_label.config(text="Enter a valid number.")
+        guess_entry.delete(0, tk.END) # deletes the entry in the box
+        return
+    
+    if guess < 1 or guess > 100:
+        result_label.config(text="Enter a number from 1 to 100 only.")
+        return
 
     # if-elif-else block
     if guess > secret_number:
         result_label.config(text="Guess is too high")
     elif guess < secret_number:
-        result_label.config(teext="Guess is too low")
+        result_label.config(text="Guess is too low")
     else:
         result_label.config(text="You got it!!!")
+    guess_entry.delete(0, tk.END)
 
 # title_label
 title_label = tk.Label(window, text="Guess the Number", font=("Arial", 20, "bold"))
@@ -43,4 +52,5 @@ guess_button.pack(pady=10)
 result_label = tk.Label(window, text="", font=("Arial", 12))
 result_label.pack(pady=20)
 
+guess_entry.bind("<Return>", show_guess)
 window.mainloop() # starts the window
