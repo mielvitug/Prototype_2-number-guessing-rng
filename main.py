dbb77d9d23b45d723b1d6aacbddf893d787f9280
mielvitug <atvitug1@gmail.com>
2026-04-17 18:44:08 +0800

    Update

--- main.py
+++ main.py
@@ -3,26 +3,43 @@
 import random # imports the python dependencies
 import tkinter as tk # GUI library
 
+# intialize
 window = tk.Tk() # creates the main app window
 window.title("Number Guessing Game") # Title inside the top of the window
 window.geometry("500x700") # Starting size of the window
 
+secret_number = random.randint(1,100)
+
+# function show_guess
 def show_guess():
     guess = guess_entry.get()
     result_label.config(text=f"You typed: {guess}")
 
+    # if-elif-else block
+    if guess > secret_number:
+        result_label.config(text="Guess is too high")
+    elif guess < secret_number:
+        result_label.config(teext="Guess is too low")
+    else:
+        result_label.config(text="You got it!!!")
+
+# title_label
 title_label = tk.Label(window, text="Guess the Number", font=("Arial", 20, "bold"))
 title_label.pack(pady=20)
 
-instruction_label = tk.Label(window, text="Enter a number from 1 to 100", font=("Arial", 12))
-instruction_label.pack()
+# instructions_label
+instructions_label = tk.Label(window, text="Enter any number from 1 to 100: ", font=("Arial", 12))
+instructions_label.pack()
 
+# guess_entry
 guess_entry = tk.Entry(window, font=("Arial", 14))
 guess_entry.pack(pady=10)
 
+# guess_button
 guess_button = tk.Button(window, text="Submit Guess", command=show_guess)
 guess_button.pack(pady=10)
 
+# result_label
 result_label = tk.Label(window, text="", font=("Arial", 12))
 result_label.pack(pady=20)
 
